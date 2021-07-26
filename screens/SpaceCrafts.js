0b6a02f3ac3bb744b23c4aaee849c957b9d69931
praadhithya <59948300+praadhithya@users.noreply.github.com>
2021-07-26 19:04:18 +0530

    Add files via upload

---
+++ screens/SpaceCrafts.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {  Text, View,StyleSheet,TouchableOpacity,ImageBackground,SafeAreaView } from 'react-native';
+export default class SpaceCrafts extends Component {
+    render(){
+        return (
+        <View style={{
+            flex:1,
+            justifyContent:"center",
+            alignItems:"center"
+        }}>
+        <Text>SpaceCrafts</Text>
+        </View>
+        )
+    }
+}
